4aa668478d42b9c24cc71a047b1b521dc5cf8d6a
wintergalt <wintergalt@gmail.com>
2011-10-26 11:40:11 -0300

    Tutorial finished, but without the las 'generic views' part.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 
-from django.http import HttpResponse
-#from django.template import Context,loader
-from polls.models import Poll
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from polls.models import Poll,Choice
 from django.shortcuts import render_to_response,get_object_or_404
 
 #def index(request):
@@ -23,10 +24,29 @@ def index(request):
     
 def detail(request,poll_id):
     p = get_object_or_404(Poll,pk=poll_id)
-    return render_to_response('polls/detail.html',{'poll':p})
+    return render_to_response('polls/detail.html',{'poll':p},
+                              context_instance=RequestContext(request))
 
 def results(request,poll_id):
+    p = get_object_or_404(Poll,pk=poll_id)
+    return render_to_response('polls/results.html', {'poll':p})
     return HttpResponse('You are looking at the results of poll %s.' % poll_id)
 
 def vote(request,poll_id):
-    return HttpResponse('You are voting on poll %s.' % poll_id)
+    p = get_object_or_404(Poll,pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form
+        return render_to_response('polls/detail.html', {
+            'poll':p,
+            'error_message':'You did not select a choice',
+            }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+    
